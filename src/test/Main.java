352db4820626c98cc471037b580dea6eeb66a30d
LYQ <1635269861@qq.com>
2025-08-13 22:39:04 +0800

    初始环境搭建

---
+++ src/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+import com.sun.javafx.tools.packager.MakeAllParams;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Map<String,Object> map = new HashMap<>();
+        map.put("key","value");
+    }
+
+}
